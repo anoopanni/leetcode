98e8ca95ff1fd174133694703c8675d924867c64
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-04-24 19:03:14 -0700

    Update jumpGame.py

--- jumpGame.py
+++ jumpGame.py
@@ -1,7 +1,6 @@
 # 55. Jump Game
 # https://leetcode.com/problems/jump-game/description/
 
-
 from functools import lru_cache
 
 class Solution:
@@ -42,28 +41,55 @@ class Solution:
 
         # Solution 3 - Greedy approach 
 
-        n = len(nums)
+        # n = len(nums)
 
-        goal = n-1
+        # goal = n-1
 
-        for i in range(n-2, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i 
+        # for i in range(n-2, -1, -1):
+        #     if i + nums[i] >= goal:
+        #         goal = i 
 
-        return goal == 0
+        # return goal == 0
 
 
         # Solution 4 - Greedy approach 2
 
-#         a = 0
-#         for val in nums:
-#             if a < 0:
-#                 return False
-#             a = max(a, val)
-#             a -= 1
-#         return True
+        # a = 0
+        # for val in nums:
+        #     if a < 0:
+        #         return False
+        #     a = max(a, val)
+        #     a -= 1
+        # return True
 
 
+        # Solution 5 - Greedy approach 3
+
+        n = len(nums)
+        reachable = 0
+
+        for i in range(n):
+
+            if reachable < i:
+                return False
+
+            reachable = max(reachable, nums[i]+i)
+
+        return True
+
+
+
+# Example 1:
+
+# Input: nums = [2,3,1,1,4]
+# Output: true
+# Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+    
+# Example 2:
+
+# Input: nums = [3,2,1,0,4]
+# Output: false
+# Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 
 
