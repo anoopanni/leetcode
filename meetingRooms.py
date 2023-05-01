b5a52c7e7c476ddd168508d9b779c7b3fe2d4a51
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-01 15:06:08 -0700

    Update meetingRooms.py

--- meetingRooms.py
+++ meetingRooms.py
@@ -3,16 +3,31 @@
 
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
 
+        # Solution 1
+
+        # intervals = sorted(intervals, key=lambda x:x[0])
+
+        # for i in range(len(intervals)-1):
+        #     if intervals[i][0] <= intervals[i+1][0] < intervals[i][1]:
+        #         return False
+
+        # return True
+
+
+        # Solution 2 
 
         intervals = sorted(intervals, key=lambda x:x[0])
 
-        for i in range(len(intervals)-1):
-            if intervals[i][0] <= intervals[i+1][0] < intervals[i][1]:
+        for i in range(1, len(intervals)):
+            if intervals[i-1][1] > intervals[i][0]:
                 return False
-            
+
         return True
-      
+
+
       
 
       
