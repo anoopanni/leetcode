8e9ebbe38cfafae7f54b4f9054c5ab5d72d1f8a1
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-04 14:01:16 -0700

    Create insertInterval.py

---
+++ insertInterval.py
@@ -0,0 +1,34 @@
+# 57. Insert Interval
+# https://leetcode.com/problems/insert-interval/
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+            
+        res.append(newInterval)
+
+        return res
+        
+        
+        
+# Example 1:
+
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+  
+  
+# Example 2:
+
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
