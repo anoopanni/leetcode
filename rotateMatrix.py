dbc8050489741fc133a024382b6a7d6ed28d4716
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-05-10 01:13:35 -0700

    Update rotateMatrix.py

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -1,13 +1,16 @@
+# 48. Rotate Image
+# https://leetcode.com/problems/rotate-image/description/
+
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
 
+        # Solution 1 - using 4 pointer approach 
+
         l, r = 0, len(matrix) - 1
         
-
-
         while l < r:
             t, b = l, r
 
@@ -28,9 +31,39 @@ class Solution:
                 # put back topLeft element
                 matrix[t+i][r] = topLeft
 
-
             l += 1
             r -= 1
+
+
+        
+        # Solution 2 - Reversing rows and transpose(internchanging rows and columns)
+
+        # rows, cols = len(matrix), len(matrix[0])
+        # t, b = 0, rows-1
+
+        # while t < b:
+        #   matrix[t], matrix[b] = matrix[b], matrix[t]
+        #   t += 1
+        #   b -= 1
+
+        # for i in range(rows):
+        #     for j in range(i):
+        #         matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+          
+
+        # Solution 3 - Interchanging row elements with column elements (Doesn't work in-place)
+        # O(n^2) extra memory 
+
+        # rows, cols = len(matrix), len(matrix[0])
+        # n_matrix = [[0 for j in range(cols)] for i in range(rows)]
+
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         n_matrix[j][cols-i-1] = matrix[i][j]
+        
+        # matrix = n_matrix
+        
+            
 
             
 # Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
